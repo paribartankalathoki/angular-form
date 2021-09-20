19a390035a2b2b5d2fae660aafd89121a9e448de
Paribartan Kalathoki <paribartan.kalathoki@sbsolutionsnepal.com>
2021-09-20 23:11:17 +0545

    added basic form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Angular Form Exampleas
+# Angular Form Examples
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.2.
 
--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./reactive-form.component.scss']
 })
 export class ReactiveFormComponent implements OnInit {
+  form: any;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.form = this.formBuilder.group({
+      fullName: [undefined],
+      email: [undefined],
+      contactNumber: [undefined],
+      permanentAddress: new FormGroup({
+        address: new FormControl(),
+        country: new FormControl(),
+        state: new FormControl(),
+        city: new FormControl(),
+      }),
+      currentAddress: new FormGroup({
+        address: new FormControl(),
+        country: new FormControl(),
+        state: new FormControl(),
+        city: new FormControl(),
+      }),
+    });
+  }
+
+  onSubmit() {
+    console.log('form data: ', this.form.value);
   }
 
 }
